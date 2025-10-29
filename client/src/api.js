2bb42c84e4c7e9a8f42872a16afd6bafc6e97755
habibi <asurax1983@gmail.com>
2025-10-29 11:01:38 +0530

    Update api.js

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://hostel-backend-asurax.onrender.com/api",
+  baseURL: "https://hostel-bobh.onrender.com/api",
   withCredentials: true,
 });
 
